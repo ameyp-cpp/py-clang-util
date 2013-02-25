bf78cef68677d9dfd4ec9cf67f9ea37cabc1bedd
Amey Parulekar <amey@wirywolf.com>
2013-02-25 12:07:40 +0530

    Corrected line and column numbers.

--- sublime.py
+++ sublime.py
@@ -44,47 +44,48 @@ class View:
     def __init__(self, file_name, line_num, col_num):
         self._file = open(file_name, 'r')
 
-        content = self._file.readlines()
-        for i in range(1, line_num):
+        for i in range(0, line_num):
             self._file.readline()
 
-        self._sel = [Selection(Region(self._file.tell(), self._file.tell() + col_num - 1))]
+        self._sel = [Region(self._file.tell(), self._file.tell() + col_num)]
         self._settings = {}
 
     def file_name(self):
         return self._file.name
 
-    def line(position):
+    def line(self, position):
         self._file.seek(0)
         content = self._file.read(position)
         beg = content.rfind('\n') + 1
-        return Region(beg, position)
+        self._file.readline()
+        end = self._file.tell() - 1
+        return Region(beg, end)
 
     def substr(self, region):
         self._file.seek(region.begin())
         content = self._file.read(region.end() - region.begin())
         return content
 
-    def rowcoll(self, position):
+    def rowcol(self, position):
         self._file.seek(0)
         content = self._file.read(position)
         newlines = content.count('\n')
 
         self._file.seek(0)
-        for i in range(1, newlines):
+        for i in range(0, newlines):
             self._file.readline()
 
         column = position - self._file.tell()
-        return newlines + 1, column + 1
+        return newlines, column
 
     def scope_name(self, position):
         # Doesn't do anything
         return "source.c++"
 
-    def is_dirty():
+    def is_dirty(self):
         return False
 
-    def is_scratch():
+    def is_scratch(self):
         return False
 
     def sel(self):
