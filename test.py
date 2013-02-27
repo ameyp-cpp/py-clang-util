8ce1b05affd4f7a45b7d5c15678354f5516dd20d
Amey Parulekar <amey@wirywolf.com>
2013-02-28 00:13:21 +0530

    Use different paths based on hostname.

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import sublimeclang
 from sublime import View
 
+import socket
+
 completions = []
 
-flags = ["-I/home/aparulekar/Developer/GamePlay/gameplay/src","-I/home/aparulekar/Developer/GamePlay/external-deps/bullet/include","-I/home/aparulekar/Developer/GamePlay/external-deps/oggvorbis/include","-I/home/aparulekar/Developer/GamePlay/external-deps/libpng/include","-I/home/aparulekar/Developer/GamePlay/external-deps/zlib/include","-I/home/aparulekar/Developer/GamePlay/external-deps/lua/include","-I/home/aparulekar/Developer/GamePlay/external-deps/glew/include"]
-filename = "/home/aparulekar/Developer/GamePlay/gameplay-samples/sample00-mesh/src/MeshGame.cpp"
-position = 1300
+if socket.gethostname() == "firefly-lin":
+    flags = ["-I/home/amey/Developer/GamePlay/gameplay/src","-I/home/amey/Developer/GamePlay/external-deps/bullet/include","-I/home/amey/Developer/GamePlay/external-deps/oggvorbis/include","-I/home/amey/Developer/GamePlay/external-deps/libpng/include","-I/home/amey/Developer/GamePlay/external-deps/zlib/include","-I/home/amey/Developer/GamePlay/external-deps/lua/include","-I/home/amey/Developer/GamePlay/external-deps/glew/include"]
+    filename = "/home/amey/Developer/GamePlay/Projects/Genesis/src/GenesisGame.cpp"
+else:
+    flags = ["-I/home/aparulekar/Developer/GamePlay/gameplay/src","-I/home/aparulekar/Developer/GamePlay/external-deps/bullet/include","-I/home/aparulekar/Developer/GamePlay/external-deps/oggvorbis/include","-I/home/aparulekar/Developer/GamePlay/external-deps/libpng/include","-I/home/aparulekar/Developer/GamePlay/external-deps/zlib/include","-I/home/aparulekar/Developer/GamePlay/external-deps/lua/include","-I/home/aparulekar/Developer/GamePlay/external-deps/glew/include"]
+    filename = "/home/aparulekar/Developer/GamePlay/Projects/Genesis/src/GenesisGame.cpp"
+
+position = 1060
 prefix = ""
 view = View(filename, position, flags)
-folders = ["/home/aparulekar/Developer/GamePlay"]
+folders = ["/home/amey/Developer/GamePlay"]
 
 def found_completions(completions):
-    print completions
+    print len(completions)
 
 scaa = sublimeclang.SublimeClangAutoComplete()
 scaa.on_query_completions(view, prefix, [position], found_completions)
