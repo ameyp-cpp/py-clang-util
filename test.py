ee9d88dfc3e8b21ee477ab6650657e6507674bd9
Amey Parulekar <amey@wirywolf.com>
2013-02-28 17:22:02 +0530

    Test warming up cache.

--- test.py
+++ test.py
@@ -8,19 +8,23 @@ completions = []
 if socket.gethostname() == "firefly-lin":
     flags = ["-I/home/amey/Developer/GamePlay/gameplay/src","-I/home/amey/Developer/GamePlay/external-deps/bullet/include","-I/home/amey/Developer/GamePlay/external-deps/oggvorbis/include","-I/home/amey/Developer/GamePlay/external-deps/libpng/include","-I/home/amey/Developer/GamePlay/external-deps/zlib/include","-I/home/amey/Developer/GamePlay/external-deps/lua/include","-I/home/amey/Developer/GamePlay/external-deps/glew/include"]
     filename = "/home/amey/Developer/GamePlay/Projects/Genesis/src/GenesisGame.cpp"
+    folders = ["/home/amey/Developer/GamePlay"]
 else:
     flags = ["-I/home/aparulekar/Developer/GamePlay/gameplay/src","-I/home/aparulekar/Developer/GamePlay/external-deps/bullet/include","-I/home/aparulekar/Developer/GamePlay/external-deps/oggvorbis/include","-I/home/aparulekar/Developer/GamePlay/external-deps/libpng/include","-I/home/aparulekar/Developer/GamePlay/external-deps/zlib/include","-I/home/aparulekar/Developer/GamePlay/external-deps/lua/include","-I/home/aparulekar/Developer/GamePlay/external-deps/glew/include"]
-    filename = "/home/aparulekar/Developer/GamePlay/Projects/Genesis/src/GenesisGame.cpp"
+    filename = "/home/aparulekar/Developer/GamePlay/gameplay-samples/sample00-mesh/src/MeshGame.cpp"
+    folders = ["/home/aparulekar/Developer/GamePlay"]
 
 position = 1060
 prefix = ""
 view = View(filename, position, flags)
-folders = ["/home/amey/Developer/GamePlay"]
 
 def found_completions(completions):
     print len(completions)
 
 scaa = sublimeclang.SublimeClangAutoComplete()
+print "Warming up cache."
+scaa.warmup_cache(view)
+print "Getting completions."
 scaa.on_query_completions(view, prefix, [position], found_completions)
 
 def found_definition(target):
